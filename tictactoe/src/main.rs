9451caff2f2928a1032d2f0cfe8ffca87af07395
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2024-10-04 23:38:01 +0200

    Create first logic with verification per slice and grid

---
+++ tictactoe/src/main.rs
@@ -0,0 +1,129 @@
+
+use ndarray::{prelude::*, ViewRepr};
+
+type Point = i8;
+type Grid = ndarray::ArrayBase<ndarray::OwnedRepr<Point>, ndarray::Dim<[usize; 2]>>;
+type GridSlice<'a> = ArrayBase<ViewRepr<&'a Point>, Dim<[usize; 1]>>;
+
+#[derive(Debug, PartialEq)]
+enum Player {
+    Human,
+    CPU,
+}
+
+type Winner = Player;
+
+fn create_grid() -> Grid {
+    Array::zeros((3, 3))
+}
+
+fn is_winning_grid(grid: Grid) -> Option<Winner> {
+    let lines = grid.sum_axis(Axis(1));
+    let columns = grid.sum_axis(Axis(0));
+
+    let diag = grid.diag().sum();
+
+    let anti_diag = (0..=2).map(|x| grid[[x, 2 - x]]).sum::<Point>();
+
+    for val in [lines.to_vec(), columns.to_vec(), vec![diag, anti_diag]].concat() {
+        match val {
+            3 => return Some(Winner::Human),
+            -3 => return Some(Winner::CPU),
+            _ => (),
+        }
+    }
+
+    None
+}
+
+fn is_winning_sequence(slice: GridSlice) -> Option<Winner> {
+    match slice.sum() {
+        3 => Some(Winner::Human),
+        -3 => Some(Winner::CPU),
+        _ => None,
+    }
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_an_empty_grid() {
+        let empty_grid = create_grid();
+        assert_eq!(empty_grid, Array::zeros((3, 3)));
+        assert_eq!(empty_grid, array![[0, 0, 0], [0, 0, 0], [0, 0, 0],]);
+    }
+
+    #[test]
+    fn no_wining_sequence_from_empty_line() {
+        let empty_grid = create_grid();
+        let empty_line = empty_grid.slice(s![.., 0]);
+        let result = is_winning_sequence(empty_line);
+        assert!(result.is_none());
+        let result = is_winning_sequence(array![0, 0, 0].slice(s![..]));
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn get_result_for_wining_sequence() {
+        let result = is_winning_sequence(array![1, 1, 1].slice(s![..]));
+        assert_eq!(result.unwrap(), Winner::Human);
+
+        let result = is_winning_sequence(array![-1, -1, -1].slice(s![..]));
+        assert_eq!(result.unwrap(), Winner::CPU);
+    }
+
+    #[test]
+    fn is_wining_grid_should_return_none_with_no_winner() {
+        let empty_grid = create_grid();
+
+        let result = is_winning_grid(empty_grid);
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn is_wining_grid_should_return_winner_on_lines() {
+        let grid = array![[0, 0, 0], [1, 1, 1], [0, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::Human);
+
+        let grid = array![[0, 0, 0], [-1, -1, -1], [0, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::CPU);
+    }
+    #[test]
+    fn is_wining_grid_should_return_winner_on_columns() {
+        let grid = array![[1, 0, 0], [1, 0, 0], [1, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::Human);
+
+        let grid = array![[-1, 0, 0], [-1, 0, 0], [-1, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::CPU);
+    }
+
+    #[test]
+    fn is_wining_grid_should_return_winner_on_diagonal() {
+        let grid = array![[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::Human);
+
+        let grid = array![[-1, 0, 0], [0, -1, 0], [0, 0, -1]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::CPU);
+    }
+
+    #[test]
+    fn is_wining_grid_should_return_winner_on_antidiagonal() {
+        let grid = array![[0, 0, 1], [0, 1, 0], [1, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::Human);
+
+        let grid = array![[0, 0, -1], [0, -1, 0], [-1, 0, 0]];
+        let result = is_winning_grid(grid);
+        assert_eq!(result.unwrap(), Winner::CPU);
+    }
+}
