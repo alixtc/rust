9d0ecb34ca03da2848e9701b630954b61d9a46b7
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2024-10-06 12:44:38 +0200

    Calculate wining positions for CPU

--- tictactoe/src/cpu.rs
+++ tictactoe/src/cpu.rs
@@ -1,6 +1,7 @@
 use super::grid::{extract_empty_positions, Grid};
 use ndarray::prelude::*;
 
+
 use rand::{seq::SliceRandom, thread_rng};
 
 fn make_random_move(grid: &Grid) -> Grid {
@@ -18,6 +19,45 @@ fn make_random_move(grid: &Grid) -> Grid {
     new_grid
 }
 
+
+fn is_winning_slice(slice:  ArrayBase<ndarray::ViewRepr<&i8>, Dim<[usize; 1]>>) -> bool {
+    let mut s = slice.to_owned().to_vec();
+    s.sort();
+    s == vec![-1, -1, 0] 
+}
+
+fn extract_winning_positions(grid: &Grid) -> Vec<(usize, usize)> {
+    let mut winning_position = Vec::<(usize, usize)>::new();
+
+
+
+    for ((x, y,), _) in grid.indexed_iter().filter(|(_, val)| **val == 0) {
+        if is_winning_slice(grid.row(x)) || is_winning_slice(grid.column(y))   {
+            winning_position.push((x, y));
+        }
+    }
+
+    if is_winning_slice(grid.diag()) {
+        for (idx, value) in grid.diag().indexed_iter() {
+            if *value == 0 {
+                winning_position.push((idx, idx));
+            }
+        }
+    }
+
+    let mut anti_diag = (0..=2).map(|x| grid[[x, 2 - x]]).collect::<Vec<_>>();
+    anti_diag.sort();
+    if anti_diag == vec![-1, -1, 0] {
+        for (idx, value) in anti_diag.iter().enumerate() {
+            if *value ==0 {
+                winning_position.push((idx, 2 - idx));
+            }
+        }
+    }
+
+
+    winning_position
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +75,47 @@ mod tests {
             grid.iter().filter(|x| **x == 0).count() - new_grid.iter().filter(|x| **x == 0).count();
         assert_eq!(zero_delta, 1);
         assert_eq!(new_grid.iter().filter(|x| **x == -1).count(), 2);
+    }
+
+    #[test]
+    fn extract_winning_positions_returns_empty_array_with_no_winning_position() {
+        #[rustfmt::skip]
+        let grid = array![
+            [1, -1, 1], 
+            [-1, 0, 1], 
+            [1, 1, -1]
+        ];
+        let positions = extract_winning_positions(&grid);
+        assert_eq!(positions, Vec::new())
+    
+
+    }
+
+    #[test]
+    fn extract_winning_positions_returns_array_of_tuples() {
+        #[rustfmt::skip]
+        let grid = array![
+            [1, 0, 1], 
+            [-1, 0, -1], 
+            [1, 1, -1]
+        ];
+        let positions = extract_winning_positions(&grid);
+        assert_eq!(positions, vec![(1, 1)])
+    
+
+    }
+
+    #[test]
+    fn extract_winning_positions_work_on_diagonal_and_anti_diagonal() {
+        #[rustfmt::skip]
+        let grid = array![
+            [-1, 1, -1], 
+            [1, -1, 1], 
+            [0, 0, 0]
+        ];
+        let positions = extract_winning_positions(&grid);
+        assert_eq!(positions, vec![(2, 2), (2, 0) ])
+    
+
     }
 }
