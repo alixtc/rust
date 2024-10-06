1af62c3008626c378a4fa06046c26935c30624a1
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2024-10-06 09:39:48 +0200

    Refactor random choice

--- tictactoe/src/cpu.rs
+++ tictactoe/src/cpu.rs
@@ -1,38 +1,35 @@
 use super::grid::{extract_empty_positions, Grid};
+use ndarray::prelude::*;
 
-use rand::thread_rng;
+use rand::{seq::SliceRandom, thread_rng};
 
 fn make_random_move(grid: &Grid) -> Grid {
     let empty_position = extract_empty_positions(grid);
     let mut rng = thread_rng();
-    let positions = rand::seq::IteratorRandom::choose(empty_position.values(), &mut rng).unwrap();
+    let random_positions = empty_position
+        .values()
+        .collect::<Vec<_>>()
+        .choose(&mut rng)
+        .unwrap()
+        .to_owned();
+
     let mut new_grid = grid.clone();
-    new_grid[*positions] = -1;
+    new_grid[*random_positions] = -1;
     new_grid
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::prelude::*;
-    fn dummy_grid() -> Grid {
+
+    #[test]
+    fn make_random_move_should_add_minus_one() {
         #[rustfmt::skip]
         let grid = array![
             [0, 0, 1], 
-            [0, 1, 0], 
+            [0, -1, 0], 
             [1, 0, 0]
         ];
-        grid
-    }
-
-    #[test]
-    fn it_works() {
-        #[rustfmt::skip]
-            let grid = array![
-                [0, 0, 1], 
-                [0, -1, 0], 
-                [1, 0, 0]
-            ];
         let new_grid = make_random_move(&grid);
         let zero_delta =
             grid.iter().filter(|x| **x == 0).count() - new_grid.iter().filter(|x| **x == 0).count();
