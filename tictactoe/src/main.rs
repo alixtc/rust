f5ad0ac11cd1a23bea58b7e28b44b2f61ff764b6
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2025-11-08 10:58:20 +0100

    Add cpu move to main()

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -1,17 +1,24 @@
 use grid::*;
-use std::io::{self};
+use std::io::{self, Write};
 
 mod cpu;
 mod grid;
 mod mocktest;
 
+fn main() {
+    main_menu();
+    let empty_grid = grid::create_grid();
+    println!("{}", empty_grid.is_grid_full());
+    println!("{:?}", empty_grid.is_winning_grid());
+
+    let available_positions = empty_grid.extract_empty_positions();
+    println!("{available_positions:?}");
+    let new_grid = cpu::make_cpu_move(&empty_grid, cpu::Difficulty::Low);
+    println!("New Grid: {new_grid:?}");
+}
+
 fn main_menu() {
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Unable to read user input");
-
+    let input = ask_for_input();
     let input = input.trim();
     match input {
         "d" => (),
@@ -20,22 +27,15 @@ fn main_menu() {
     }
 }
 
-fn main() {
-    let empty_grid = grid::create_grid();
-    println!("{}", empty_grid.is_grid_full());
-    println!("{:?}", empty_grid.is_winning_grid());
-
-    let available_positions = extract_empty_positions(&empty_grid);
-    println!("{available_positions:?}")
-}
-
-fn ask_for_input() {
+fn ask_for_input() -> String {
+    io::stdout()
+        .write_all(b"Please select an option from the menu:\n")
+        .unwrap();
     let mut input = String::new();
-
     io::stdin()
         .read_line(&mut input)
         .expect("Unable to read user input");
-    println!("{input}")
+    input.to_owned()
 }
 
 pub fn ask_for_position<R>(mut reader: R) -> String
