f148b09d62ac4ccf314f25f5d9fd722912178a33
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2025-11-11 12:42:36 +0100

    Create skeleton of game flow and leadearboard

--- tictactoe/src/main.rs
+++ tictactoe/src/main.rs
@@ -7,35 +7,89 @@ mod cpu;
 mod grid;
 mod mocktest;
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Leaderboard {
+    cpu: i32,
+    player: i32,
+}
+
+impl Leaderboard {
+    fn update_score(&self, player: Option<Player>) -> Leaderboard {
+        if player.is_none() {
+            return self.to_owned();
+        }
+        let winner = player.unwrap();
+        match winner {
+            Player::Cpu => Leaderboard {
+                cpu: self.cpu + 1,
+                player: self.player,
+            },
+            Player::Human => Leaderboard {
+                cpu: self.cpu,
+                player: self.player + 1,
+            },
+        }
+    }
+}
+
 fn main() {
     main_menu();
-    let empty_grid = grid::create_grid();
-    println!("Is grid full? {}", empty_grid.is_grid_full());
-    println!("Is grid wining? {:?}", empty_grid.is_winning_grid());
+}
 
-    let available_positions = empty_grid.extract_empty_positions();
-    println!("{available_positions:?}");
-    let new_grid = cpu::make_cpu_move(&empty_grid, cpu::Difficulty::Low);
-    println!("New Grid:\n{}", new_grid.render());
+fn print_main_screen_menu(selected_difficulty: cpu::Difficulty) {
+    println!(
+        "
+Please select something from main menu!
+1 (s) - Start Game
+2 (d) - Set Difficulty (currently selected: {difficulty:?})
+3 (l) - Leaderboard and Score Display
+4 (q) - Quit
+",
+        difficulty = selected_difficulty
+    );
 }
 
 fn main_menu() {
-    let selected_difficulty = get_user_input_with(
-        parse_difficulty,
-        || io::stdin().lock(),
-    );
+    let mut selected_difficulty = cpu::Difficulty::Medium;
+    let mut leaderboard = Leaderboard { cpu: 0, player: 0 };
+    loop {
+        print_main_screen_menu(selected_difficulty);
+        let user_input = ask_user_input(|| io::stdin().lock())
+            .trim()
+            .to_lowercase()
+            .to_owned();
 
-    println!("Selected difficulty is: {:?}", selected_difficulty);
-
-    println!("Please select something from main menu!");
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).expect("Unable to read");
-
-    match buffer.as_str() {
-        "d" => (),
-        "q" => std::process::exit(0),
-        _ => (),
+        if (user_input == "s") | (user_input == "1") {
+            let winner = play_game(selected_difficulty);
+            leaderboard = leaderboard.update_score(winner);
+        }
+        if (user_input == "d") | (user_input == "2") {
+            selected_difficulty = get_user_input_with(parse_difficulty, || io::stdin().lock());
+        }
+        if (user_input == "q") | (user_input == "4") {
+            std::process::exit(0);
+        }
     }
+}
+
+fn play_game(difficulty: cpu::Difficulty) -> Option<grid::Player> {
+    println!("Starting a new game!");
+    let mut game_grid = grid::create_grid();
+
+    while !game_grid.is_grid_full() {
+        game_grid = grid::make_user_turn(&game_grid, || io::stdin().lock());
+
+        if game_grid.is_grid_full() || game_grid.is_winning_grid().is_some() {
+            return game_grid.is_winning_grid();
+        }
+
+        game_grid = cpu::make_cpu_move(&game_grid, difficulty);
+
+        if game_grid.is_grid_full() || game_grid.is_winning_grid().is_some() {
+            return game_grid.is_winning_grid();
+        }
+    }
+    None
 }
 
 fn get_user_input_with<F, G, R, T>(parser: F, mut reader: G) -> T
@@ -51,13 +105,13 @@ where
     input.unwrap()
 }
 
-pub fn ask_for_position<R>(mut reader: R) -> String
-// Required to facilitate testing
+pub fn ask_user_input<G, R>(mut reader: G) -> String
 where
+    G: FnMut() -> R,
     R: io::BufRead,
 {
     let mut buffer = String::new();
-    reader.read_line(&mut buffer).expect("Unable to read");
+    reader().read_line(&mut buffer).expect("Unable to read");
     buffer
 }
 
@@ -67,8 +121,8 @@ mod tests {
 
     #[test]
     fn test_request_user_input() {
-        let dummy_input = b"I'm George";
-        let answer = ask_for_position(&dummy_input[..]);
+        let mock_reader = move || "I'm George".as_bytes();
+        let answer = ask_user_input(mock_reader);
         assert_eq!("I'm George", answer);
     }
 
@@ -81,11 +135,24 @@ mod tests {
             std::io::Cursor::new(input_str.as_bytes())
         };
 
-        let selected_difficulty = get_user_input_with(
-            cpu::parse_difficulty,
-            get_mock_reader,
-        );
+        let selected_difficulty = get_user_input_with(cpu::parse_difficulty, get_mock_reader);
 
         assert_eq!(selected_difficulty, crate::cpu::Difficulty::Low);
+    }
+
+    #[test]
+    fn update_score_should_not_change_without_player() {
+        let board = Leaderboard { cpu: 1, player: 3 };
+        let player: Option<Player> = None;
+        assert_eq!(board, board.update_score(player));
+    }
+
+    #[test]
+    fn update_score_should_increase_score_for_winning_player() {
+        let board = Leaderboard { cpu: 1, player: 3 };
+        let player = Some(Player::Human);
+        assert_eq!(&board.player + 1, board.update_score(player).player);
+        let player = Some(Player::Cpu);
+        assert_eq!(&board.cpu + 1, board.update_score(player).cpu);
     }
 }
